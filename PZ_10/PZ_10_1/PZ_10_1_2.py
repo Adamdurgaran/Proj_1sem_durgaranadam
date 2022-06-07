0b3aa4eb1804e6f609a40a071e14a97e8d6a614a
Adamdurgaran <93992181+Adamdurgaran@users.noreply.github.com>
2022-06-08 00:28:22 +0300

    Delete PZ_10 directory

--- PZ_10/PZ_10_1/PZ_10_1_2.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-
-file = open('file1.txt', 'w+')
-numbers = []
-for i in range(randint(1, 20)):
-    numbers.append(randint(-100, 100))
-numbers = ' '.join(map(str, numbers))
-file.write(numbers)
-file.close()
